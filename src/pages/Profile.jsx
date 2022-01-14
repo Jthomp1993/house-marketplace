cba35989307ca277e55673ef18561455d574a5b9
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-01-14 15:31:37 +0000

    User logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,32 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getAuth } from 'firebase/auth';
 
 function Profile() {
     const auth = getAuth();
-    const [user, setUser] = useState(null);
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
+    });
 
-    useEffect(() => {
-        setUser(auth.currentUser);
-    }, [])
-    return user ? <h1>{user.displayName}</h1> : 'You must be logged in to view your profile'
+    const { name, email } = formData;
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        auth.signOut();
+
+        navigate('/');
+    }
+    
+    return <div className="profile">
+        <header className="profileHeader">
+            <p className="pageHeader">My Profile</p>
+            <button type="button" className="logOut" onClick={onLogout}>
+                Logout
+            </button>
+        </header>
+    </div>
     
 }
 
