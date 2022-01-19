795fa8d4435387be4a1c9e5cf480d571b09d3f0b
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-01-19 14:56:31 +0000

    Contact landlord page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ReactToastify, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
-
 import NavBar from './components/NavBar';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
@@ -14,6 +13,7 @@ import SignUp from './pages/SignUp';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 
 function App() {
@@ -31,6 +31,7 @@ function App() {
                 <Route path='/sign-up' element={<SignUp />} />
                 <Route path='/forgot-password' element={<ForgotPassword />} />
                 <Route path='/create-listing' element={<CreateListing />} />
+                <Route path='/contact/:landlordId' element={<Contact />} />
                 <Route path='/category/:categoryName/:listingId' element={<Listing />} />
             </Routes>
             <NavBar />
