331a83eed49d280a3b78ca6174be55c68d1bc3f3
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-01-19 11:37:47 +0000

    Set formDataCopy location

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -136,7 +136,6 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude;
             geolocation.lng = longitude;
-            location = address;
         }
 
         // Store images in firebase
@@ -191,9 +190,9 @@ function CreateListing() {
             timestamp: serverTimestamp()
         }
 
+        formDataCopy.location = address;
         delete formDataCopy.images;
         delete formDataCopy.address;
-        location && (formDataCopy.location = location);
         !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
         const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
